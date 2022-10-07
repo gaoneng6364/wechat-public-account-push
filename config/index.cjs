97d34d87af3200405645b0e730d5f757cdff1dad
gaoneng6364 <107535044+gaoneng6364@users.noreply.github.com>
2022-10-07 18:14:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyFvL5xsKjddpqTChm3MbrgiMPdU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Z6rMWFtUk_0U06A_sQG0DZaOAFSPvHXsTbR71D0dXj4',
+      useTemplateId: '	pU5GLlmuQjFllvjU1P78ZEtXPj3y7XaqjsrqG5YZ8ds',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
